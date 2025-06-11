59cdd85a09f5a33ac477bc8af2a43a1495fc5a20
lfq <lfq@example.com>
2025-06-11 15:29:40 +0800

    use 6g

--- radio/DPDKRF/dpdkrf_oai.c
+++ radio/DPDKRF/dpdkrf_oai.c
@@ -56,7 +56,7 @@ int g_rate = 1;
 static int64_t freq_tx  = -1;
 static int64_t freq_rx  = -1;
 static int64_t freq_offset  = 0;
-int64_t freq_air = 5900000000;//5.9g
+int64_t freq_air = 6000000000;//5.9g
 
 int32_t test_dpdkrf_read (openair0_device *device, openair0_timestamp *ptimestamp, void **buff1, int nsamps, int cc){
 
