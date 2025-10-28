46b8dcca34b0e1d11571603ec997414713d18796
oiwireless <2025110235@bupt.cn>
2025-10-28 18:52:53 +0800

    ming verify that the queue is not empty.

--- common/utils/threadPool/notified_fifo.h
+++ common/utils/threadPool/notified_fifo.h
@@ -224,7 +224,7 @@ static inline void abortNotifiedFIFO(notifiedFIFO_t *nf)
 // notified_fifo.h
 static inline bool notifiedFIFO_has_data(notifiedFIFO_t *nf)
 {
-  bool ret = true;
+  bool ret = false;
   mutexlock(nf->lockF);
   ret = (!nf->abortFIFO && nf->outF != NULL);
   mutexunlock(nf->lockF);
