6f9f888beed528599228fb6ca8ac5247f07c7204
ming0526288 <1971678626@qq.com>
2025-11-01 15:54:41 +0800

    ming set a gnb tun.

--- openair2/SDAP/nr_sdap/nr_sdap.c
+++ openair2/SDAP/nr_sdap/nr_sdap.c
@@ -393,7 +393,7 @@ void start_direct_sdap_tun_gnb_first_ue_default_pdu_session(ue_id_t ue_id)
   DevAssert(entity->is_gnb);//Æô¶¯»ùÕ¾²àµÄÐéÄâÍø¿¨
   char *ifprefix = get_softmodem_params()->nsa ? "oaitun_gnb" : "oaitun_enb";
   char ifname[IFNAMSIZ];
-  tun_generate_ifname(ifname, ifprefix, ue_id - 1);
+  tun_generate_ifname(ifname, ifprefix, 0);
   entity->pdusession_sock = tun_alloc(ifname);
   tun_config(ifname, "192.169.0.1", NULL);
   threadCreate(&entity->pdusession_thread, sdap_direct_tun_read_thread, entity, "gnb_tun_read_thread", -1, OAI_PRIORITY_RT_LOW);
